f2c9752bc79d8f8e377569791f157478075132ae
Owner <nadavhalvey@gmail.com>
2022-02-09 16:33:39 +0200

    Commit test cases for the code

--- main.py
+++ main.py
@@ -67,24 +67,25 @@ def output_results_to_console(list_of_collateral_adjective, animal_array):
 
 
 # __main__
-soup = make_soup(WIKI_URL)
-# find and Scraping second table
-table = soup.find_all('table', class_="wikitable")[1]
-# Collecting data:
-collateral_adjective_list = create_a_collateral_adjective_list(table)
-# sort the list of collateral adjective, delete duplicate and delete unnecessary characters
-collateral_adjective_list = list(dict.fromkeys(sorted(collateral_adjective_list.split())))[6:-1]
-# sort the list of animal & collateral adjective together
-animals_array = create_an_animal_array(table)
+if __name__ == "__main__":
+    soup = make_soup(WIKI_URL)
+    # find and Scraping second table
+    table = soup.find_all('table', class_="wikitable")[1]
+    # Collecting data:
+    collateral_adjective_list = create_a_collateral_adjective_list(table)
+    # sort the list of collateral adjective, delete duplicate and delete unnecessary characters
+    collateral_adjective_list = list(dict.fromkeys(sorted(collateral_adjective_list.split())))[6:-1]
+    # sort the list of animal & collateral adjective together
+    animals_array = create_an_animal_array(table)
 
-# work with Thread
-start1 = time.perf_counter()
-with ThreadPoolExecutor(8) as executor:
-    results = executor.map(output_results_to_an_html_file(collateral_adjective_list, animals_array))
-finish1 = time.perf_counter()
-print(f"Finished with threading in {round(finish1 - start1, 2)} seconds")
-# work without Thread
-start2 = time.perf_counter()
-output_results_to_console(collateral_adjective_list, animals_array)
-finish2 = time.perf_counter()
-print(f"Finished without threading in {round(finish2 - start2, 2)} seconds")
+    # work with Thread
+    start1 = time.perf_counter()
+    with ThreadPoolExecutor(8) as executor:
+        results = executor.map(output_results_to_an_html_file(collateral_adjective_list, animals_array))
+    finish1 = time.perf_counter()
+    print(f"Finished with threading in {round(finish1 - start1, 2)} seconds")
+    # work without Thread
+    start2 = time.perf_counter()
+    output_results_to_console(collateral_adjective_list, animals_array)
+    finish2 = time.perf_counter()
+    print(f"Finished without threading in {round(finish2 - start2, 2)} seconds")
